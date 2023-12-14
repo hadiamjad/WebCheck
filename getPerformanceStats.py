6890fa3c64b2d148d0d224144c98026fc7510607
Hadi Amjad <hadiamjad@Hadis-MacBook-Air.local>
2023-12-14 18:47:26 -0500

    performance stats

--- getPerformanceStats.py
+++ getPerformanceStats.py
@@ -1,6 +1,7 @@
 import os
 import json
 import numpy as np
+import statistics
 
 # Function to remove outliers using the IQR method
 def remove_outliers(data):
@@ -29,28 +30,36 @@ for folder in folders:
             for key in values_output:
                 if key in data:
                     values_output[key].append(data[key])
+
         with open(output_surr_path) as file:
             data = json.load(file)
             for key in values_output_surr:
                 if key in data:
                     values_output_surr[key].append(data[key])
 
-# Compute and print the averages for output after removing outliers
-print("Averages for output (Outliers Removed):")
+# Function to calculate mean, median, and mode
+def calculate_statistics(data):
+    if not data:
+        return "No data"
+    try:
+        mean = np.mean(data)
+        median = np.median(data)
+        mode = statistics.mode(data)
+        return mean, median, mode
+    except statistics.StatisticsError:
+        # If there's no unique mode
+        return mean, median, "No unique mode"
+
+# Compute and print the statistics for output after removing outliers
+print("Statistics for output (Outliers Removed):")
 for key in values_output:
     filtered_values = remove_outliers(values_output[key])
-    if filtered_values:
-        average = sum(filtered_values) / len(filtered_values)
-        print(f"{key}: {average}")
-    else:
-        print(f"{key}: No data or all data are outliers")
+    stats = calculate_statistics(filtered_values)
+    print(f"{key} - Mean: {stats[0]}, Median: {stats[1]}, Mode: {stats[2]}")
 
-# Compute and print the averages for output_surr after removing outliers
-print("Averages for output_surr (Outliers Removed):")
+# Compute and print the statistics for output_surr after removing outliers
+print("Statistics for output_surr (Outliers Removed):")
 for key in values_output_surr:
     filtered_values = remove_outliers(values_output_surr[key])
-    if filtered_values:
-        average = sum(filtered_values) / len(filtered_values)
-        print(f"{key}: {average}")
-    else:
-        print(f"{key}: No data or all data are outliers")
+    stats = calculate_statistics(filtered_values)
+    print(f"{key} - Mean: {stats[0]}, Median: {stats[1]}, Mode: {stats[2]}")
