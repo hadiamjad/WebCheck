cde94ae610ae22bb1be6cf559654a1c8e60bc0d9
Hadi Amjad <hadiamjad@Hadis-MacBook-Air.local>
2023-12-16 13:23:33 -0500

    minor changes

--- sele.py
+++ sele.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 import time
 import json
 import shutil
-# from pyvirtualdisplay import Display
+from pyvirtualdisplay import Display
 import pandas as pd
 import requests
 import os
@@ -19,8 +19,8 @@ import chromedriver_autoinstaller
 
 
 # virtual display
-# display = Display(visible=0, size=(1000, 1000))
-# display.start()
+display = Display(visible=0, size=(1000, 1000))
+display.start()
 
 df = pd.read_csv(r"ten.csv")
 # extractDigits(os.listdir('/home/student/TrackerSift/UserStudy/output'))
